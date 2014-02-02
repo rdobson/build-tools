f3087f4abd88f544a3f0c5d26298dd2d5edbb68d
Robert Breker <robert.breker@citrix.com>
2014-02-02 13:46:16 +0000

    Still a long way to go
    Signed-off-by: Robert Breker <robert.breker@citrix.com>

--- run-on-vm.sh
+++ run-on-vm.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 set -eux
 
-if [ "$#" -ne 3 ]; then
+if [ "$#" -le 4 ]; then
     echo "Invalid parameters"
-    echo "Usage: $0 vmuuid password script"
+    echo "Usage: $0 vmuuid password script xenserver_host xenserver_password"
     exit 1
 fi
 
+. ./functions.sh
+
 VMUUID=$1
 PASSWORD=$2
-SCRIPT=$3
+XENSERVER_HOST=$3
+XENSERVER_PASSWORD=$4
+SCRIPT=$5
+
+XENSERVER_USERNAME="root"
 
 ip=""
 while [[ $ip == "" ]]; do
     sleep 1
-    ip=$(xe vm-param-get uuid="$VMUUID" param-name=networks | sed -ne 's,^.*0/ip: \([0-9.]*\).*$,\1,p')
+    ip=$(xecommand vm-param-get uuid="$VMUUID" param-name=networks | sed -ne 's,^.*0/ip: \([0-9.]*\).*$,\1,p')
     if [[ $ip != "" ]]; then
-        if [[ $(ping -c 1 "$ip" | grep bytes | wc -l) -le 1 ]]; then
+        if [[ $(sshcommand "echo ok") != "ok" ]]; then
             ip=""
         fi
     fi
 done
 
-sshpass -p "$PASSWORD" scp -o StrictHostKeyChecking=no $SCRIPT root@"$HOST":~/
-$basename=$(basename "$SCRIPT")
-sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no root@"$HOST" "$basename $CCP $SYSTEMVM" 
+tmpdir=$(sshcommand mktemp -d)
+sshpass -p "$PASSWORD" scp -o StrictHostKeyChecking=no -r ./ root@"$ip":"$tmpdir"
+basename=$(basename "$SCRIPT")
+sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no root@"$ip" "$tmpdir/$basename" "${@:6}"
